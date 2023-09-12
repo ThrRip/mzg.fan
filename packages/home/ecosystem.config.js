d0b3c0ca0ae9b06439a07baa5300ba5f4405344e
ThrRip <coding@thrrip.space>
2023-09-12 16:49:25 +0800

    home: Restart the process when consuming 150M or more memory

--- packages/home/ecosystem.config.js
+++ packages/home/ecosystem.config.js
@@ -2,10 +2,11 @@ module.exports = {
   apps: [
     {
       name: 'mzg.fan-home',
+      script: './.output/server/index.mjs',
       port: 22320,
       exec_mode: 'cluster',
       instances: 'max',
-      script: './.output/server/index.mjs',
+      max_memory_restart: '150M',
       env: {
         'NITRO_PORT': 22320
       }
