8a1abc8c9493916d98bfa0636b62c8ecc1b9e48c
ThrRip <coding@thrrip.space>
2023-09-21 18:35:18 +0800

    [all]: Enforce the JavaScript Standard Style ESLint rules
    ... with some overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,12 @@ module.exports = {
     node: true
   },
   extends: [
+    'standard',
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended'
   ],
   rules: {
-    'indent': ['error', 2, { ignoredNodes: ['IfStatement > :matches([alternate])'] }],
+    'indent': ['error', 2, { ignoredNodes: ['IfStatement[alternate.type="IfStatement"]'], SwitchCase: 1 }],
     'operator-linebreak': ['error', 'after'],
     'quote-props': ['error', 'consistent']
   }
