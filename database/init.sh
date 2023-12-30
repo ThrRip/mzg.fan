4bd8afbfc899c1d71fc2964764270471381edf29
ThrRip <coding@thrrip.space>
2023-12-30 17:40:17 +0800

    Add schema and scripts for backend database initialization

---
+++ database/init.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+_PrintUsage() {
+    printf "\
+Usage:  ENDPOINT=<endpoint> PROJECT_ID=<projectId> KEY=<key> ./init.sh
+        ./init.sh --endpoint <endpoint> --projectId <projectId> --key <key>
+
+Initialize the database of your Appwrite project.
+
+Arguments:
+  <endpoint>     Appwrite server endpoint
+  <projectId>    Appwrite project ID
+  <key>          Appwrite API key
+"
+    exit 1
+}
+
+[ ! -z "$(echo "$@" | grep -Eo "\--endpoint \S+")" ] &&
+    ENDPOINT="$(echo "$@" | grep -Eo "\--endpoint \S+" | cut -d ' ' -f2)" ||
+    [ ! -z "$ENDPOINT" ]  ||
+    _PrintUsage
+
+[ ! -z "$(echo "$@" | grep -Eo "\--projectId \S+")" ] &&
+    PROJECT_ID="$(echo "$@" | grep -Eo "\--projectId \S+" | cut -d ' ' -f2)" ||
+    [ ! -z "$PROJECT_ID" ]  ||
+    _PrintUsage
+
+[ ! -z "$(echo "$@" | grep -Eo "\--key \S+")" ] &&
+    KEY="$(echo "$@" | grep -Eo "\--key \S+" | cut -d ' ' -f2)" ||
+    [ ! -z "$KEY" ]  ||
+    _PrintUsage
+
+_fail() {
+    printf "\n- $1\n"
+    exit $2
+}
+_exec_dry() {
+    _command="$@"
+    printf "\n+ $_command\n"
+}
+_exec() {
+    _command="$@"
+    _exec_dry $_command
+    "$@"
+    _exit_code=$?
+    [ $_exit_code != 0 ] && _fail "The command of \"""$_command""\" failed with code $_exit_code. Terminating the script." $_exit_code
+}
+_exec_quiet() {
+    _command="$@"
+    "$@"
+    _exit_code=$?
+    [ $_exit_code != 0 ] && _fail "The command of \"""$_command""\" failed with code $_exit_code. Terminating the script." $_exit_code
+}
+
+_exec_dry appwrite client --endpoint "$ENDPOINT" --projectId "$PROJECT_ID" --key "[REDACTED]"
+_exec_quiet appwrite client --endpoint "$ENDPOINT" --projectId "$PROJECT_ID" --key "$KEY"
+
+_exec appwrite databases create --databaseId home --name "Home"
+_exec appwrite databases create --databaseId admin --name "Administration Panel"
+
+_exec appwrite deploy collection --all --yes
