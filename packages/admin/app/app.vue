b659eec4b7035de9de4f501410a17e93b134426e
ThrRip <coding@thrrip.space>
2025-01-08 07:57:09 +0800

    Update copyright year

--- packages/admin/app/app.vue
+++ packages/admin/app/app.vue
@@ -1,5 +1,5 @@
 <!--
-  - Copyright (c) 2023-2024 ThrRip, XMing (洺知-故犯)
+  - Copyright (c) 2023-2025 ThrRip, XMing (洺知-故犯)
   -
   - This Source Code Form is subject to the terms of the Mozilla Public
   - License, v. 2.0. If a copy of the MPL was not distributed with this
